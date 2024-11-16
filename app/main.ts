f3cb50b810b2d3bc620a3619c64828dc7caec86d
Joshua Speelman <joshua.m.speelman@gmail.com>
2024-11-16 13:41:07 +1000

    [test message]

--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,17 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data: String) => {
+    const requestString = data;
+
+    // need to get the path from the first line
+    const path = data.split("\r\n")[0];
+
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
   });
 
   socket.on("close", () => {
