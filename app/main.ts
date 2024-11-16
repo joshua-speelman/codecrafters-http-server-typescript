a5a2efd73836355061fb305497dbd261b908f544
Joshua Speelman <joshua.m.speelman@gmail.com>
2024-11-16 13:43:50 +1000

    [test message]

--- app/main.ts
+++ app/main.ts
@@ -5,11 +5,9 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
-    const requestString = data.ToString();
-
+  socket.on("data", (data: String) => {
     // need to get the path from the first line
-    const path = requestString.split("\r\n")[0];
+    const path = data.split("\r\n")[0];
 
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
