686b0d81323e76d881ffc26e1d64155899c05301
Joshua Speelman <joshua.m.speelman@gmail.com>
2024-11-16 13:50:30 +1000

    [test message]

--- app/main.ts
+++ app/main.ts
@@ -11,7 +11,7 @@ const server = net.createServer((socket) => {
     const path = requestString.split("\r\n")[0];
     console.log(path);
 
-    if (path === "/") {
+    if (requestString === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
